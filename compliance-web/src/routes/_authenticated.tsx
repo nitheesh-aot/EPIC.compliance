4e8dd1408e785a4248602405f565ab706940bdee
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2024-07-30 19:30:45 -0600

    authentication changes

--- compliance-web/src/routes/_authenticated.tsx
+++ compliance-web/src/routes/_authenticated.tsx
@@ -1,10 +1,19 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { useEffect } from "react";
+import { useAuth } from "react-oidc-context";
 
-export const Route = createFileRoute('/_authenticated')({
-  beforeLoad: ({ context }) => {
-    const { isAuthenticated, signinRedirect } = context.authentication;
-    if (!isAuthenticated) {
+export const Route = createFileRoute("/_authenticated")({
+  component: Auth,
+});
+
+function Auth() {
+  const { isAuthenticated, signinRedirect, isLoading } = useAuth();
+
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) {
       signinRedirect();
     }
-  },
-})
+  }, [isAuthenticated, isLoading, signinRedirect]);
+
+  return <Outlet />;
+}
