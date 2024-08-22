dc92af5c83d5fa5be8cd24f790943c28be48f7f5
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2024-08-22 01:10:57 -0600

    unit tests for store and utils

---
+++ compliance-web/src/store/__test__/snackbarStore.cy.tsx
@@ -0,0 +1,53 @@
+/// <reference types="cypress" />
+import React from "react";
+import { mount } from "cypress/react18";
+import { useSnackbar, notify } from "../snackbarStore";
+
+describe("Snackbar Zustand Store", () => {
+  // A simple React component to access and display the state
+  const TestComponent: React.FC = () => {
+    const { isOpen, severity, message, setClose } = useSnackbar();
+
+    return (
+      <div>
+        {isOpen && (
+          <div className={`snackbar snackbar-${severity}`}>
+            <span>{message}</span>
+            <button onClick={setClose}>Close</button>
+          </div>
+        )}
+      </div>
+    );
+  };
+
+  beforeEach(() => {
+    mount(<TestComponent />);
+  });
+
+  it("should open the snackbar with success severity", () => {
+    notify.success("Success message");
+    cy.get(".snackbar-success").should("exist").and("contain", "Success message");
+  });
+
+  it("should open the snackbar with error severity", () => {
+    notify.error("Error message");
+    cy.get(".snackbar-error").should("exist").and("contain", "Error message");
+  });
+
+  it("should open the snackbar with warning severity", () => {
+    notify.warning("Warning message");
+    cy.get(".snackbar-warning").should("exist").and("contain", "Warning message");
+  });
+
+  it("should open the snackbar with info severity", () => {
+    notify.info("Info message");
+    cy.get(".snackbar-info").should("exist").and("contain", "Info message");
+  });
+
+  it("should close the snackbar", () => {
+    notify.success("Close test");
+    cy.get(".snackbar-success").should("exist").and("contain", "Close test");
+    cy.get("button").click();
+    cy.get(".snackbar").should("not.exist");
+  });
+});
