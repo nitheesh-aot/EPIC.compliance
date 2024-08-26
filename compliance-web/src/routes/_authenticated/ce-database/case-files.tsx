25d91249815ede8b1415bdc4c4135f07f4d3b9d4
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2024-08-26 15:53:11 -0600

    case files grid

--- compliance-web/src/routes/_authenticated/ce-database/case-files.tsx
+++ compliance-web/src/routes/_authenticated/ce-database/case-files.tsx
@@ -1,45 +1,264 @@
 import CaseFileDrawer from "@/components/App/CaseFiles/CaseFileDrawer";
+import TableFilter from "@/components/Shared/FilterSelect/TableFilter";
+import MasterDataTable from "@/components/Shared/MasterDataTable/MasterDataTable";
+import { searchFilter } from "@/components/Shared/MasterDataTable/utils";
+import { useCaseFilesData } from "@/hooks/useCaseFiles";
+import { CaseFile } from "@/models/CaseFile";
+import { StaffUser } from "@/models/Staff";
 import { useDrawer } from "@/store/drawerStore";
 import { notify } from "@/store/snackbarStore";
-import { AddRounded } from "@mui/icons-material";
-import { Box, Button, Typography } from "@mui/material";
+import dateUtils from "@/utils/dateUtils";
+import {
+  AddRounded,
+  DeleteOutlineRounded,
+  EditOutlined,
+} from "@mui/icons-material";
+import { Box, Button, Chip, IconButton, Typography } from "@mui/material";
+import { useQueryClient } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { BCDesignTokens } from "epic.theme";
+import { MRT_ColumnDef } from "material-react-table";
+import { useEffect, useMemo, useState } from "react";
 
 export const Route = createFileRoute("/_authenticated/ce-database/case-files")({
   component: CaseFiles,
 });
 
 function CaseFiles() {
+  const queryClient = useQueryClient();
   const { setOpen, setClose } = useDrawer();
+  const { data: caseFilesList, isLoading } = useCaseFilesData();
+
+  const [projectList, setProjectList] = useState<string[]>([]);
+  const [initiationList, setInitiationList] = useState<string[]>([]);
+  const [statusList, setStatusList] = useState<string[]>([]);
+  const [staffUserList, setStaffUserList] = useState<string[]>([]);
+
+  useEffect(() => {
+    setProjectList(
+      [...new Set(caseFilesList?.map((cf) => cf.project?.name ?? ""))].filter(
+        Boolean
+      )
+    );
+    setInitiationList(
+      [...new Set(caseFilesList?.map((cf) => cf.initiation ?? ""))].filter(
+        Boolean
+      )
+    );
+    setStatusList(
+      [
+        ...new Set(
+          caseFilesList?.map((cf) => (cf.is_active ? "Active" : "Inactive"))
+        ),
+      ].filter(Boolean)
+    );
+    setStaffUserList(
+      [
+        ...new Set(
+          caseFilesList?.map((cf) => cf.lead_officer?.full_name ?? "")
+        ),
+      ].filter(Boolean)
+    );
+  }, [caseFilesList]);
 
   const handleOpenModal = () => {
     setOpen(<CaseFileDrawer onSubmit={handleOnSubmit} />);
   };
 
   const handleOnSubmit = (submitMsg: string) => {
+    queryClient.invalidateQueries({ queryKey: ["case-files"] });
     setClose();
     notify.success(submitMsg);
   };
 
+  const handleEdit = (staff: StaffUser) => {
+    //TODO: EDIT
+    // eslint-disable-next-line no-console
+    console.log(staff);
+  };
+
+  const handleDelete = (id: number) => {
+    //TODO: DELETE
+    // eslint-disable-next-line no-console
+    console.log(id);
+  };
+
+  const columns = useMemo<MRT_ColumnDef<CaseFile>[]>(
+    () => [
+      {
+        accessorKey: "case_file_number",
+        header: "Case File #",
+        sortingFn: "sortFn",
+        filterFn: searchFilter,
+      },
+      {
+        accessorKey: "project.name",
+        header: "Project",
+        filterVariant: "multi-select",
+        filterSelectOptions: projectList,
+        Filter: ({ header, column }) => {
+          return (
+            <TableFilter
+              isMulti
+              header={header}
+              column={column}
+              variant="inline"
+              name="projectFilter"
+              placeholder="Filter Projects"
+            />
+          );
+        },
+      },
+      {
+        accessorKey: "initiation",
+        header: "Initiation",
+        filterVariant: "multi-select",
+        filterSelectOptions: initiationList,
+        Filter: ({ header, column }) => {
+          return (
+            <TableFilter
+              isMulti
+              header={header}
+              column={column}
+              variant="inline"
+              name="initiationFilter"
+              placeholder="Filter Initiations"
+            />
+          );
+        },
+      },
+      {
+        accessorFn: (row) => dateUtils.formatDate(row.date_created),
+        id: "date_created",
+        header: "Date Created",
+      },
+      {
+        accessorKey: "is_active",
+        header: "Status",
+        Cell: ({ row }) => {
+          return row.original.is_active ? (
+            <Chip
+              label="Active"
+              color="success"
+              variant="outlined"
+              size="small"
+            />
+          ) : (
+            <Chip
+              label="Inactive"
+              color="error"
+              variant="outlined"
+              size="small"
+            />
+          );
+        },
+        filterVariant: "multi-select",
+        filterSelectOptions: statusList,
+        Filter: ({ header, column }) => {
+          return (
+            <TableFilter
+              isMulti
+              header={header}
+              column={column}
+              variant="inline"
+              name="statusFilter"
+              placeholder="Filter Status"
+            />
+          );
+        },
+        filterFn: (row, id, filterValue) => {
+          if (
+            !filterValue.length ||
+            filterValue.length > statusList.length // select all is selected
+          ) {
+            return true;
+          }
+          return filterValue.includes(
+            row.getValue(id) || false ? "Active" : "Inactive"
+          );
+        },
+      },
+      {
+        accessorFn: (row) => row.lead_officer?.full_name,
+        id: "lead_officer.full_name",
+        header: "Lead Officer",
+        filterVariant: "multi-select",
+        filterSelectOptions: staffUserList,
+        Filter: ({ header, column }) => {
+          return (
+            <TableFilter
+              isMulti
+              header={header}
+              column={column}
+              variant="inline"
+              name="leadOfficersFilter"
+              placeholder="Filter Lead Officers"
+            />
+          );
+        },
+      },
+    ],
+    [initiationList, projectList, staffUserList, statusList]
+  );
+
   return (
-    <Box
-      sx={{
-        width: "100%",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
-    >
-      <Typography
-        variant="h5"
-        sx={{ color: BCDesignTokens.typographyColorLink }}
-      >
-        Case Files
-      </Typography>
-      <Button startIcon={<AddRounded />} onClick={() => handleOpenModal()}>
-        Case File
-      </Button>
-    </Box>
+    <>
+      <MasterDataTable
+        columns={columns}
+        data={caseFilesList ?? []}
+        initialState={{
+          sorting: [
+            {
+              id: "case_file_number",
+              desc: false,
+            },
+          ],
+        }}
+        state={{
+          isLoading: isLoading,
+          showGlobalFilter: true,
+        }}
+        enableRowActions={true}
+        renderRowActions={({ row }) => (
+          <Box gap={".25rem"} display={"flex"}>
+            <IconButton
+              aria-label="edit"
+              onClick={() => handleEdit(row.original)}
+            >
+              <EditOutlined />
+            </IconButton>
+            <IconButton
+              aria-label="delete"
+              onClick={() => handleDelete(row.original.id)}
+            >
+              <DeleteOutlineRounded />
+            </IconButton>
+          </Box>
+        )}
+        renderTopToolbarCustomActions={() => (
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              variant="h5"
+              sx={{ color: BCDesignTokens.typographyColorLink }}
+            >
+              Case Files
+            </Typography>
+            <Button
+              startIcon={<AddRounded />}
+              onClick={() => handleOpenModal()}
+            >
+              Case File
+            </Button>
+          </Box>
+        )}
+      />
+    </>
   );
 }
