f65e079a9b54145dd4c5627b2bf91e42e24804ca
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2024-08-26 15:10:47 -0600

    case_file_number and inititation api data corrected

--- compliance-web/src/models/CaseFile.ts
+++ compliance-web/src/models/CaseFile.ts
@@ -7,7 +7,8 @@ export interface CaseFile {
   project_id: number;
   date_created: string;
   lead_officer_id: number;
-  case_file_number: number;
+  case_file_number: string;
+  initiation: string;
   is_active: boolean;
   project: Project;
   lead_officer: StaffUser;
@@ -19,7 +20,7 @@ export interface CaseFileFormData {
   lead_officer?: StaffUser;
   officers?: StaffUser[];
   initiation?: Initiation;
-  case_file_number?: number;
+  case_file_number?: string;
 }
 
 export interface CaseFileAPIData {
@@ -27,6 +28,6 @@ export interface CaseFileAPIData {
   date_created: string;
   lead_officer_id?: number;
   officer_ids?: number[];
-  initiation: string;
-  case_file_number: number;
+  initiation_id: string;
+  case_file_number: string;
 }
