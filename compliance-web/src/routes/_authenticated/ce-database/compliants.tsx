c74c104c4db9607cde57a48990251d86513001aa
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2024-09-16 14:53:36 -0600

    COMP-123: Create Complaint - Default Page

--- compliance-web/src/routes/_authenticated/ce-database/compliants.tsx
+++ compliance-web/src/routes/_authenticated/ce-database/compliants.tsx
@@ -1,10 +1,35 @@
 import { createFileRoute } from "@tanstack/react-router";
-import ComingSoon from "@/components/Shared/ComingSoon";
+import { AddRounded } from "@mui/icons-material";
+import { Box, Typography, Button } from "@mui/material";
+import { BCDesignTokens } from "epic.theme";
 
 export const Route = createFileRoute("/_authenticated/ce-database/compliants")({
   component: Compliance,
 });
 
 function Compliance() {
-  return <ComingSoon />
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <Typography
+        variant="h5"
+        sx={{ color: BCDesignTokens.typographyColorLink }}
+      >
+        Complaints
+      </Typography>
+      <Button
+        id="addActionButton"
+        startIcon={<AddRounded />}
+        // onClick={}
+      >
+        Complaint
+      </Button>
+    </Box>
+  );
 }
