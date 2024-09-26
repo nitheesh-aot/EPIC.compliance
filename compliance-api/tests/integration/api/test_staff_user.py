086e6ce83a8b738bf55ccbe8ad8bd73ab1fb5736
dinesh <dinesh.pb@aot-technologies.com>
2024-09-26 14:10:32 -0700

    adding tests

--- compliance-api/tests/integration/api/test_staff_user.py
+++ compliance-api/tests/integration/api/test_staff_user.py
@@ -71,65 +71,40 @@ def test_get_users(mock_auth_service, mocker, client, auth_header):
     """Create an existing user."""
     url = urljoin(API_BASE_URL, "staff-users")
     user_data = StaffScenario.default_data.value
-    auth_user_guid1 = "dinesh1"  # fake.text(max_nb_chars=20)
+    auth_user_guid1 = fake.text(max_nb_chars=20)
     user_data["auth_user_guid"] = auth_user_guid1
     StaffScenario.create(user_data)
-    auth_user_guid2 = "dinesh2"  # fake.text(max_nb_chars=20)
+    auth_user_guid2 = fake.text(max_nb_chars=20)
     user_data["auth_user_guid"] = auth_user_guid2
     StaffScenario.create(user_data)
     mock_get_epic_users_by_app = mocker.patch(
         "compliance_api.services.authorize_service.auth_service.AuthService.get_epic_users_by_app"
     )
-    epic_users = [
-        {
-            "id": "5bca57e4-1b39-498c-8872-732d3da4beaf",
-            "first_name": "Shaelyn",
-            "last_name": "Tolk",
-            "email_address": "shaelyn.tolk@gov.bc.ca",
-            "username": f"{auth_user_guid1}",
-            "groups": [
-                {
-                    "id": "b872cb25-2a8b-4b98-8aff-1c8ac1c47b40",
-                    "name": "VIEWER",
-                    "path": "COMPLIANCE/VIEWER",
-                    "level": 0,
-                    "display_name": "",
-                },
-                {
-                    "id": "6f94f250-c4bf-4dc6-9158-fc64e8083edc",
-                    "name": "USER",
-                    "path": "COMPLIANCE/USER",
-                    "level": 1,
-                    "display_name": "",
-                },
-            ],
-        },
-        {
-            "id": "34413651-fd5a-4253-b205-5acac3611915",
-            "first_name": "Abhinav",
-            "last_name": "Sharma",
-            "email_address": "abhinav.sharma@gov.bc.ca",
-            "username": f"{auth_user_guid2}",
-            "groups": [
-                {
-                    "id": "6f94f250-c4bf-4dc6-9158-fc64e8083edc",
-                    "name": "USER",
-                    "path": "COMPLIANCE/USER",
-                    "level": 1,
-                    "display_name": "",
-                }
-            ],
-        },
-    ]
+    epic_users = StaffScenario.epic_auth_users.value
+    epic_users[0]["username"] = auth_user_guid1
+    epic_users[1]["username"] = auth_user_guid2
+
     mock_get_epic_users_by_app.return_value = epic_users
 
     result = client.get(url, headers=auth_header)
-    print(result.json)
     filtered_user = next(
         (user for user in result.json if user["auth_user_guid"] == auth_user_guid1),
         None,
     )
-
+    print(filtered_user)
     assert filtered_user is not None
-    assert filtered_user["permission"] == "USER"
+    assert filtered_user.get("permission", None) == "USER"
     assert result.status_code == HTTPStatus.OK
+
+
+def test_get_user_by_id(mock_auth_service, client, auth_header):
+    """Get user by id"""
+    staff_data = StaffScenario.default_data.value
+    staff_data["auth_user_guid"]="dinesh"
+    created_user = StaffScenario.create(staff_data)
+    url = urljoin(API_BASE_URL, f"staff-users/{created_user.id}")
+
+    result = client.get(url, headers=auth_header)
+
+    assert result.status_code == HTTPStatus.OK
+    assert result.json["id"] == created_user.id
