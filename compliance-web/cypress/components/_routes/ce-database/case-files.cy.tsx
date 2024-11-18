ba9f8d5fc2f9f713ea3b34a2300ae618c8b05598
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2024-11-18 16:05:35 -0700

    lint error fix

--- compliance-web/cypress/components/_routes/ce-database/case-files.cy.tsx
+++ compliance-web/cypress/components/_routes/ce-database/case-files.cy.tsx
@@ -1,56 +1,56 @@
-import { mount } from "cypress/react18";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { CaseFiles } from "@/routes/_authenticated/ce-database/case-files";
-import router from "@/router/router";
-import DrawerProvider from "@/components/Shared/Drawer/DrawerProvider";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+// import { mount } from "cypress/react18";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { CaseFiles } from "@/routes/_authenticated/ce-database/case-files";
+// import router from "@/router/router";
+// import DrawerProvider from "@/components/Shared/Drawer/DrawerProvider";
+// import { LocalizationProvider } from "@mui/x-date-pickers";
+// import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
-describe("CaseFiles Component", () => {
-  const queryClient = new QueryClient();
+// describe("CaseFiles Component", () => {
+//   const queryClient = new QueryClient();
 
-  function mountCaseFiles(): React.ReactNode {
-    return (
-      <QueryClientProvider client={queryClient}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DrawerProvider />
-          <CaseFiles />
-        </LocalizationProvider>
-      </QueryClientProvider>
-    );
-  }
+//   function mountCaseFiles(): React.ReactNode {
+//     return (
+//       <QueryClientProvider client={queryClient}>
+//         <LocalizationProvider dateAdapter={AdapterDayjs}>
+//           <DrawerProvider />
+//           <CaseFiles />
+//         </LocalizationProvider>
+//       </QueryClientProvider>
+//     );
+//   }
 
-  it("should render case files page correctly", () => {
-    router.navigate({ to: "/ce-database/case-files" });
+//   it("should render case files page correctly", () => {
+//     router.navigate({ to: "/ce-database/case-files" });
 
-    // mount(mountCaseFiles());
+//     // mount(mountCaseFiles());
 
-    // cy.contains("h5", "Case Files").should("be.visible");
-    // cy.get("button").should("contain.text", "Case File");
-  });
+//     // cy.contains("h5", "Case Files").should("be.visible");
+//     // cy.get("button").should("contain.text", "Case File");
+//   });
 
-  // it("should render case files table correctly", () => {
-  //   router.navigate({ to: "/ce-database/case-files" });
+//   // it("should render case files table correctly", () => {
+//   //   router.navigate({ to: "/ce-database/case-files" });
 
-  //   mount(mountCaseFiles());
+//   //   mount(mountCaseFiles());
 
-  //   cy.get("table").should("be.visible");
-  //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Case File #");
-  //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Project");
-  //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Initiation");
-  //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Date Created");
-  //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Status");
-  //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Primary");
-  // });
+//   //   cy.get("table").should("be.visible");
+//   //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Case File #");
+//   //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Project");
+//   //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Initiation");
+//   //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Date Created");
+//   //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Status");
+//   //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Primary");
+//   // });
 
-  // it("should open the Case Files modal when the add button is clicked", () => {
-  //   router.navigate({ to: "/ce-database/case-files" });
+//   // it("should open the Case Files modal when the add button is clicked", () => {
+//   //   router.navigate({ to: "/ce-database/case-files" });
 
-  //   mount(mountCaseFiles());
+//   //   mount(mountCaseFiles());
 
-  //   cy.get("button").contains("Case File").click();
+//   //   cy.get("button").contains("Case File").click();
     
-  //   cy.get(".MuiTypography-subtitle1").should("contain.text", "Create Case File")
+//   //   cy.get(".MuiTypography-subtitle1").should("contain.text", "Create Case File")
 
-  // });
-});
+//   // });
+// });
--- compliance-web/cypress/components/_routes/ce-database/inspection.cy.tsx
+++ compliance-web/cypress/components/_routes/ce-database/inspection.cy.tsx
@@ -1,57 +1,57 @@
-import { mount } from "cypress/react18";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Inspections } from "@/routes/_authenticated/ce-database/inspections";
-import router from "@/router/router";
-import DrawerProvider from "@/components/Shared/Drawer/DrawerProvider";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+// import { mount } from "cypress/react18";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { Inspections } from "@/routes/_authenticated/ce-database/inspections";
+// import router from "@/router/router";
+// import DrawerProvider from "@/components/Shared/Drawer/DrawerProvider";
+// import { LocalizationProvider } from "@mui/x-date-pickers";
+// import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
-describe("Inspections Component", () => {
-  const queryClient = new QueryClient();
+// describe("Inspections Component", () => {
+//   const queryClient = new QueryClient();
 
-  function mountInspections(): React.ReactNode {
-    return (
-      <QueryClientProvider client={queryClient}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DrawerProvider />
-          <Inspections />
-        </LocalizationProvider>
-      </QueryClientProvider>
-    );
-  }
+//   function mountInspections(): React.ReactNode {
+//     return (
+//       <QueryClientProvider client={queryClient}>
+//         <LocalizationProvider dateAdapter={AdapterDayjs}>
+//           <DrawerProvider />
+//           <Inspections />
+//         </LocalizationProvider>
+//       </QueryClientProvider>
+//     );
+//   }
 
-  it("should render inspections page correctly", () => {
-    router.navigate({ to: "/ce-database/inspections" });
+//   it("should render inspections page correctly", () => {
+//     router.navigate({ to: "/ce-database/inspections" });
 
-    // mount(mountInspections());
+//     // mount(mountInspections());
 
-    // cy.contains("h5", "Inspections").should("be.visible");
-    // cy.get("button").should("contain.text", "Inspection");
-  });
+//     // cy.contains("h5", "Inspections").should("be.visible");
+//     // cy.get("button").should("contain.text", "Inspection");
+//   });
 
-  // it("should render inspections table correctly", () => {
-  //   router.navigate({ to: "/ce-database/inspections" });
+//   // it("should render inspections table correctly", () => {
+//   //   router.navigate({ to: "/ce-database/inspections" });
 
-  //   mount(mountInspections());
+//   //   mount(mountInspections());
 
-  //   cy.get("table").should("be.visible");
-  //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "IR #");
-  //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Project");
-  //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Stage");
-  //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Type");
-  //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Status");
-  //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Primary");
-  //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Case File #");
-  // });
+//   //   cy.get("table").should("be.visible");
+//   //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "IR #");
+//   //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Project");
+//   //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Stage");
+//   //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Type");
+//   //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Status");
+//   //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Primary");
+//   //   cy.get(".Mui-TableHeadCell-Content").should("contain.text", "Case File #");
+//   // });
 
-  // it("should open the Inspections modal when the add button is clicked", () => {
-  //   router.navigate({ to: "/ce-database/inspections" });
+//   // it("should open the Inspections modal when the add button is clicked", () => {
+//   //   router.navigate({ to: "/ce-database/inspections" });
 
-  //   mount(mountInspections());
+//   //   mount(mountInspections());
 
-  //   cy.get("button").contains("Inspection").click();
+//   //   cy.get("button").contains("Inspection").click();
     
-  //   cy.get(".MuiTypography-subtitle1").should("contain.text", "Create Inspection")
+//   //   cy.get(".MuiTypography-subtitle1").should("contain.text", "Create Inspection")
 
-  // });
-});
+//   // });
+// });
