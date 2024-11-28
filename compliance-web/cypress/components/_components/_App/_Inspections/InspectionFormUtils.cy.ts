3c0f3c480f131fb4b8f71335e832d9ee9542d272
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2024-11-28 15:56:43 -0700

    test fixes

--- compliance-web/cypress/components/_components/_App/_Inspections/InspectionFormUtils.cy.ts
+++ compliance-web/cypress/components/_components/_App/_Inspections/InspectionFormUtils.cy.ts
@@ -1,11 +1,7 @@
 /// <reference types="cypress" />
-import {
-  InspectionFormSchema,
-  // formatInspectionData,
-  getProjectId,
-} from "@/components/App/Inspections/InspectionFormUtils";
-import { UNAPPROVED_PROJECT_ID } from "@/utils/constants";
+import { InspectionFormSchema, formatInspectionData } from "@/components/App/Inspections/InspectionFormUtils";
 import dateUtils from "@/utils/dateUtils";
+import dayjs from "dayjs";
 
 describe("InspectionFormUtils", () => {
   describe("InspectionFormSchema Validation", () => {
@@ -61,72 +57,51 @@ describe("InspectionFormUtils", () => {
     });
   });
 
-  // describe("formatInspectionData function", () => {
-  //   it("formats data correctly for API submission", () => {
-  //     const formData = {
-  //       project: { id: 1, name: "Project Alpha" },
-  //       authorization: "Authorization 123",
-  //       regulatedParty: "Company XYZ",
-  //       projectDescription: "Project Description",
-  //       projectType: "Type A",
-  //       projectSubType: "Sub Type B",
-  //       locationDescription: "Location A",
-  //       utm: "9U 454135 6399452",
-  //       primaryOfficer: { id: 1, full_name: "John Doe" },
-  //       officers: [{ id: 2, full_name: "Jane Smith" }],
-  //       irTypes: [{ id: 1, name: "IR Type Alpha" }],
-  //       dateRange: {
-  //         startDate: dayjs("2023-01-01"),
-  //         endDate: dayjs("2023-01-02"),
-  //       },
-  //       initiation: { id: 1, name: "Initiation Alpha" },
-  //       irStatus: { id: 1, name: "Status 1" },
-  //       projectStatus: { id: 1, name: "Status 1" },
-  //       inAttendance: [{ id: 1, name: "Agency 1" }],
-  //       municipal: "Municipal Info",
-  //       other: "Other Info",
-  //       firstNations: [{ id: 1, name: "First Nation 1" }],
-  //       agencies: [{ id: 1, name: "Agency 1" }],
-  //     };
-
-  //     const expectedFormattedData = {
-  //       project_id: 1,
-  //       case_file_id: 123,
-  //       inspection_type_ids: [1],
-  //       initiation_id: 1,
-  //       start_date: dayjs("2023-01-01").toISOString(),
-  //       end_date: dayjs("2023-01-02").toISOString(),
-  //       primary_officer_id: 1,
-  //       inspection_officer_ids: [2],
-  //       location_description: "Location A",
-  //       utm: "9U 454135 6399452",
-  //       ir_status_id: 1,
-  //       project_status_id: 1,
-  //       attendance_option_ids: [1],
-  //       agency_attendance_ids: [1],
-  //       firstnation_attendance_ids: [1],
-  //       attendance_municipal: "Municipal Info",
-  //       attendance_other: "Other Info",
-  //     };
-
-  //     const formattedData = formatInspectionData(formData, 123);
-  //     expect(formattedData).to.deep.equal(expectedFormattedData);
-  //   });
-  // });
-
-  describe("getProjectId function", () => {
-    it("returns project id correctly", () => {
+  describe("formatInspectionData function", () => {
+    it("formats data correctly for API submission", () => {
       const formData = {
-        project: { id: 1, name: "Project Alpha" },
+        projectDescription: "Project Description",
+        locationDescription: "Location A",
+        utm: "9U 454135 6399452",
+        primaryOfficer: { id: 1, full_name: "John Doe" },
+        officers: [{ id: 2, full_name: "Jane Smith" }],
+        irTypes: [{ id: 1, name: "IR Type Alpha" }],
+        dateRange: {
+          startDate: dayjs("2023-01-01"),
+          endDate: dayjs("2023-01-02"),
+        },
+        initiation: { id: 1, name: "Initiation Alpha" },
+        irStatus: { id: 1, name: "Status 1" },
+        projectStatus: { id: 1, name: "Status 1" },
+        inAttendance: [{ id: 1, name: "Agency 1" }],
+        municipal: "Municipal Info",
+        other: "Other Info",
+        firstNations: [{ id: 1, name: "First Nation 1" }],
+        agencies: [{ id: 1, name: "Agency 1" }],
       };
-      expect(getProjectId(formData)).to.equal(1);
-    });
 
-    it("returns undefined for unapproved project", () => {
-      const formData = {
-        project: { id: UNAPPROVED_PROJECT_ID, name: "Unapproved Project" },
+      const expectedFormattedData = {
+        case_file_id: 123,
+        inspection_type_ids: [1],
+        initiation_id: 1,
+        project_description: "Project Description",
+        start_date: dayjs("2023-01-01").toISOString(),
+        end_date: dayjs("2023-01-02").toISOString(),
+        primary_officer_id: 1,
+        attending_officer_ids: [2],
+        location_description: "Location A",
+        utm: "9U 454135 6399452",
+        ir_status_id: 1,
+        project_status_id: 1,
+        attendance_option_ids: [1],
+        agency_attendance_ids: [1],
+        firstnation_attendance_ids: [1],
+        attendance_municipal: "Municipal Info",
+        attendance_other: "Other Info",
       };
-      expect(getProjectId(formData)).to.be.undefined;
+
+      const formattedData = formatInspectionData(formData, 123);
+      expect(formattedData).to.deep.equal(expectedFormattedData);
     });
   });
 });
