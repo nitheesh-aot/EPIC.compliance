3726016ecf0c35efc97311ec5dea5661c2ca68c4
dinesh <dinesh.pb@aot-technologies.com>
2025-01-29 13:48:40 -0800

    library version issue fix

--- compliance-api/src/compliance_api/services/staff_user.py
+++ compliance-api/src/compliance_api/services/staff_user.py
@@ -1,10 +1,6 @@
 """Service for user management."""
 
-from compliance_api.exceptions import (
-    ResourceExistsError,
-    ResourceNotFoundError,
-    UnprocessableEntityError,
-)
+from compliance_api.exceptions import ResourceExistsError, ResourceNotFoundError, UnprocessableEntityError
 from compliance_api.models.db import session_scope
 from compliance_api.models.staff_user import StaffUser as StaffUserModel
 from compliance_api.utils.constant import AUTH_APP
