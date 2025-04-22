82dcdc1109dceb966f085b2ad8b11904fdf3f435
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2025-04-22 17:10:50 -0600

    preview button group

--- compliance-web/src/components/App/Inspections/Profile/Reports/PreviewDownloadButton.tsx
+++ compliance-web/src/components/App/Inspections/Profile/Reports/PreviewDownloadButton.tsx
@@ -1,19 +1,39 @@
 import { downloadFile } from "@/utils/appUtils";
-import { PictureAsPdfOutlined } from "@mui/icons-material";
-import { Button } from "@mui/material";
-import { useState } from "react";
+import { DownloadRounded, PictureAsPdfOutlined } from "@mui/icons-material";
+import {
+  Button,
+  ClickAwayListener,
+  ButtonGroup,
+  Grow,
+  Paper,
+  Popper,
+  MenuList,
+  MenuItem,
+} from "@mui/material";
+import { useRef, useState } from "react";
 import { useInspectionRecordRender } from "@/hooks/useInspectionReports";
 import { useModal } from "@/store/modalStore";
 import { useReportStore } from "./reportStore";
 import ReportPreviewModal from "./ReportPreviewModal";
 import { notify } from "@/store/snackbarStore";
+import { ArrowDropDownIcon } from "@mui/x-date-pickers/icons";
+import { BCDesignTokens } from "epic.theme";
 
 const PreviewDownloadButton = () => {
-  const { setOpen } = useModal();
+  const { setOpen: setModalOpen } = useModal();
   const { inspectionData, inspectionReportsData } = useReportStore();
   const [previewClicked, setPreviewClicked] = useState(false);
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef<HTMLDivElement>(null);
 
-  const { refetch: refetchIrRenderData } = useInspectionRecordRender(
+  const { refetch: refetchIrPreviewData } = useInspectionRecordRender(
+    inspectionData?.id ?? 0,
+    inspectionReportsData?.id ?? 0,
+    "html",
+    false
+  );
+
+  const { refetch: refetchIrPDFData } = useInspectionRecordRender(
     inspectionData?.id ?? 0,
     inspectionReportsData?.id ?? 0,
     "pdf",
@@ -23,24 +43,15 @@ const PreviewDownloadButton = () => {
   const handlePreviewClick = async () => {
     setPreviewClicked(true);
     try {
-      const result = await refetchIrRenderData();
+      const result = await refetchIrPreviewData();
+      setPreviewClicked(false);
       if (result.data) {
-        setPreviewClicked(false);
-
-        if (result.data.html) {
-          // Handle HTML preview
-          const html = result.data.html ?? "";
-          setOpen({
-            content: <ReportPreviewModal previewHtml={html} />,
-            width: "660px",
-          });
-        } else {
-          // The result.data is a Blob because the responseType is 'blob'
-          downloadFile(
-            result.data,
-            `IR-${inspectionData?.ir_number || "inspection"}-${Date.now()}.pdf`
-          );
-        }
+        // Handle HTML preview
+        const html = result.data.html ?? "";
+        setModalOpen({
+          content: <ReportPreviewModal previewHtml={html} />,
+          width: "660px",
+        });
       }
     } catch (error) {
       notify.error("Failed to generate PDF preview");
@@ -48,16 +59,112 @@ const PreviewDownloadButton = () => {
     }
   };
 
+  const handleDownloadClick = async (event: MouseEvent) => {
+    setPreviewClicked(true);
+    handleClose(event);
+    try {
+      const result = await refetchIrPDFData();
+      setPreviewClicked(false);
+      if (result.data) {
+        downloadFile(
+          result.data,
+          `IR-${inspectionData?.ir_number || "inspection"}-${Date.now()}.pdf`
+        );
+      }
+    } catch (error) {
+      notify.error("Failed to generate PDF preview");
+      setPreviewClicked(false);
+    }
+  };
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event: MouseEvent | TouchEvent) => {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   return (
-    <Button
-      variant="text"
-      color="primary"
-      onClick={handlePreviewClick}
-      disabled={previewClicked}
-    >
-      <PictureAsPdfOutlined sx={{ mr: 1, fontSize: 20 }} />
-      {previewClicked ? "Loading..." : "Preview"}
-    </Button>
+    <>
+      <ButtonGroup
+        variant="outlined"
+        ref={anchorRef}
+        aria-label="Button group for preview and download"
+        sx={{
+          "& .MuiButtonBase-root": {
+            padding: "0.25rem 0.75rem",
+            height: "38px",
+            color: BCDesignTokens.typographyColorPrimary,
+          },
+        }}
+      >
+        <Button onClick={handlePreviewClick} disabled={previewClicked}>
+          <PictureAsPdfOutlined sx={{ mr: 1, fontSize: 20 }} />
+          {previewClicked ? "Loading..." : "Preview"}
+        </Button>
+        <Button
+          aria-controls={open ? "split-button-menu" : undefined}
+          aria-expanded={open ? "true" : undefined}
+          aria-label="download report"
+          aria-haspopup="menu"
+          onClick={handleToggle}
+          sx={{
+            padding: "0.5rem 0 !important",
+            minWidth: "2rem !important",
+          }}
+        >
+          <ArrowDropDownIcon />
+        </Button>
+      </ButtonGroup>
+      <Popper
+        sx={{
+          zIndex: 1,
+        }}
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === "bottom" ? "center top" : "center bottom",
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList id="split-button-menu" autoFocusItem>
+                  <MenuItem
+                    key="download-report-as-pdf"
+                    onClick={(e) =>
+                      handleDownloadClick(e as unknown as MouseEvent)
+                    }
+                    disabled={previewClicked}
+                    component="button"
+                  >
+                    <DownloadRounded sx={{ mr: 1, fontSize: 20 }} />
+                    {previewClicked
+                      ? "Downloading..."
+                      : "Download Report as PDF"}
+                  </MenuItem>
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </>
   );
 };
 
