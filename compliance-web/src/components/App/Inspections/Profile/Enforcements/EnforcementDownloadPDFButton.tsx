d8ae6b161c247e531e7a80752267735e09b4f7eb
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2025-05-26 11:37:59 -0600

    lint fix

--- compliance-web/src/components/App/Inspections/Profile/Enforcements/EnforcementDownloadPDFButton.tsx
+++ compliance-web/src/components/App/Inspections/Profile/Enforcements/EnforcementDownloadPDFButton.tsx
@@ -6,8 +6,6 @@ import { downloadFile } from "@/utils/appUtils";
 import { useInspectionOrderRendered } from "@/hooks/useInspectionOrders";
 import { EnforcementActionEnum } from "@/utils/constants";
 import { useInspectionWarningLetterRendered } from "@/hooks/useInspectionWarningLetters";
-import { useModal } from "@/store/modalStore";
-import ReportPreviewModal from "../Reports/ReportPreviewModal";
 
 interface EnforcementDownloadPDFButtonProps {
   inspectionId: number;
@@ -22,7 +20,6 @@ const EnforcementDownloadPDFButton = ({
   fileNumber,
   enforcementType,
 }: EnforcementDownloadPDFButtonProps) => {
-  const { setOpen: setModalOpen } = useModal();
   const [isLoading, setIsLoading] = useState(false);
 
   const { refetch: refetchOrderPDFData } = useInspectionOrderRendered(
@@ -54,25 +51,6 @@ const EnforcementDownloadPDFButton = ({
       notify.error("Failed to download PDF");
     } finally {
       setIsLoading(false);
-    }
-  };
-
-  const handlePreviewClick = async () => {
-    try {
-      const result =
-        enforcementType === EnforcementActionEnum.ORDER
-          ? await refetchOrderPDFData()
-          : await refetchWarningLetterPDFData();
-      if (result.data) {
-        // Handle HTML preview
-        const html = result.data.html ?? "";
-        setModalOpen({
-          content: <ReportPreviewModal previewHtml={html} />,
-          width: "660px",
-        });
-      }
-    } catch (error) {
-      notify.error("Failed to generate PDF preview");
     }
   };
 
