e0689d4ee39e3296b0c425f266c7b39e375fc6b4
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2025-06-18 17:47:49 -0600

    COMP-514: Inspection display for the Case File (front) - base changes only

--- compliance-web/src/components/App/CaseFiles/Profile/CaseFileInspectionsTable.tsx
+++ compliance-web/src/components/App/CaseFiles/Profile/CaseFileInspectionsTable.tsx
@@ -1,133 +1,160 @@
-import MasterDataTable from "@/components/Shared/MasterDataTable/MasterDataTable";
-import { searchFilter } from "@/components/Shared/MasterDataTable/utils";
+import GridLabelValuePair from "@/components/Shared/GridLabelValuePair";
 import { useInspectionsByCaseFileId } from "@/hooks/useInspections";
-import { Inspection } from "@/models/Inspection";
-import { Link, Chip } from "@mui/material";
+import dateUtils from "@/utils/dateUtils";
+import { ChevronRight, ExpandLessRounded } from "@mui/icons-material";
+import {
+  Link,
+  Chip,
+  Accordion,
+  AccordionSummary,
+  Box,
+  Typography,
+  AccordionDetails,
+  Grid,
+  Skeleton,
+} from "@mui/material";
 import { Link as RouterLink } from "@tanstack/react-router";
-import { MRT_ColumnDef } from "material-react-table";
-import { useState, useEffect, useMemo } from "react";
+import { BCDesignTokens } from "epic.theme";
+import { useState } from "react";
 
 const CaseFileInspectionsTable = ({ caseFileId }: { caseFileId: number }) => {
   const { data: inspections, isLoading } =
     useInspectionsByCaseFileId(caseFileId);
 
-  const [staffUserList, setStaffUserList] = useState<string[]>([]);
-  const [inspectionStatusList, setInspectionStatusList] = useState<string[]>(
-    []
+  const [expandedInspections, setExpandedInspections] = useState<Set<number>>(
+    new Set()
   );
 
-  useEffect(() => {
-    setStaffUserList(
-      [
-        ...new Set(
-          inspections?.map((insp) => insp.primary_officer?.name ?? "")
-        ),
-      ].filter(Boolean)
-    );
-    setInspectionStatusList(
-      [
-        ...new Set(inspections?.map((insp) => insp.inspection_status ?? "")),
-      ].filter(Boolean)
-    );
-  }, [inspections]);
+  const handleAccordionChange = (inspectionId: number, expanded: boolean) => {
+    setExpandedInspections((prev) => {
+      const newSet = new Set(prev);
+      if (expanded) {
+        newSet.add(inspectionId);
+      } else {
+        newSet.delete(inspectionId);
+      }
+      return newSet;
+    });
+  };
 
-  const columns = useMemo<MRT_ColumnDef<Inspection>[]>(
-    () => [
-      {
-        accessorKey: "ir_number",
-        header: "IR #",
-        filterFn: searchFilter,
-        Cell: ({ row }) => {
+  return (
+    <>
+      <Typography variant="h6" mt={2} mb={1}>
+        Inspections
+      </Typography>
+      {isLoading && <Skeleton variant="rectangular" height={100} />}
+      {inspections && inspections.length > 0 ? (
+        inspections.map((inspection, index) => {
+          const isExpanded = expandedInspections.has(inspection.id);
+
           return (
-            <Link
-              component={RouterLink}
-              to="/ce-database/inspections/$inspectionNumber"
-              params={{
-                inspectionNumber: row.original.ir_number,
+            <Accordion
+              key={inspection.id}
+              expanded={isExpanded}
+              onChange={(_, expanded) => {
+                handleAccordionChange(inspection.id, expanded);
               }}
-              underline="hover"
+              sx={{
+                marginY: "0.5rem",
+                border: `1px solid ${BCDesignTokens.surfaceColorBorderDefault}`,
+                borderRadius: BCDesignTokens.layoutBorderRadiusMedium,
+                "&.Mui-expanded": {
+                  marginY: "0.5rem",
+                },
+                "&:before": {
+                  display: "none",
+                },
+              }}
             >
-              {row.original.ir_number?.split("_").pop()}
-            </Link>
+              <AccordionSummary
+                aria-controls={`panel${index}-content`}
+                id={`inspection-panel${index}-header`}
+                sx={{
+                  backgroundColor:
+                    BCDesignTokens.surfaceColorBackgroundLightGray,
+                  borderRadius: BCDesignTokens.layoutBorderRadiusMedium,
+                  "&.Mui-expanded": {
+                    minHeight: "48px",
+                    padding: "0.875rem 1rem",
+                    borderBottom: `1px solid ${BCDesignTokens.surfaceColorBorderDefault}`,
+                    "& .MuiAccordionSummary-content": {
+                      margin: "0",
+                    },
+                  },
+                  "& .MuiAccordionSummary-content": {
+                    display: "flex",
+                    justifyContent: "space-between",
+                    gap: "1rem",
+                  },
+                }}
+              >
+                <Box display={"flex"} alignItems={"center"} gap={0.5}>
+                  {isExpanded ? <ExpandLessRounded /> : <ChevronRight />}
+                  <Link
+                    component={RouterLink}
+                    to="/ce-database/inspections/$inspectionNumber"
+                    params={{
+                      inspectionNumber: inspection.ir_number,
+                    }}
+                    underline="hover"
+                  >
+                    {inspection.ir_number}
+                  </Link>
+                  <Chip
+                    label={inspection.inspection_status}
+                    color={
+                      inspection.inspection_status?.toLowerCase() === "open"
+                        ? "success"
+                        : "error"
+                    }
+                    variant="outlined"
+                    size="small"
+                  />
+                </Box>
+                <Box display={"flex"} alignItems={"center"} gap={0.5}>
+                  <Typography
+                    variant="body2"
+                    color={BCDesignTokens.typographyColorPlaceholder}
+                    mr={0.25}
+                  >
+                    Primary:
+                  </Typography>
+                  <Typography variant="body2">
+                    {inspection.primary_officer?.name || "N/A"}
+                  </Typography>
+                </Box>
+              </AccordionSummary>
+              <AccordionDetails sx={{ padding: "1rem" }}>
+                <Grid container spacing={2}>
+                  <GridLabelValuePair
+                    label="Location"
+                    value={inspection.location_description || "N/A"}
+                    gridProps={{ xs: 8 }}
+                  />
+                  <GridLabelValuePair
+                    label="Date"
+                    value={dateUtils.formatDate(inspection.start_date)}
+                    gridProps={{ xs: 2 }}
+                  />
+                  <GridLabelValuePair
+                    label="Status"
+                    value={inspection.inspection_status || "N/A"}
+                    gridProps={{ xs: 2 }}
+                  />
+                </Grid>
+              </AccordionDetails>
+            </Accordion>
           );
-        },
-        size: 70,
-      },
-      {
-        accessorKey: "inspection_status",
-        header: "Status",
-        Cell: ({ row }) => {
-          return row.original.inspection_status ? (
-            <Chip
-              label={row.original.inspection_status}
-              color={
-                row.original.inspection_status?.toLowerCase() === "open"
-                  ? "success"
-                  : "error"
-              }
-              variant="outlined"
-              size="small"
-            />
-          ) : (
-            <></>
-          );
-        },
-        filterVariant: "multi-select",
-        filterSelectOptions: inspectionStatusList,
-        size: 100,
-      },
-      // TODO: Add map correct values for the next 3 columns
-      {
-        accessorKey: "subtopic",
-        header: "Subtopic",
-        size: 120,
-      },
-      {
-        accessorKey: "source",
-        header: "Source",
-        size: 120,
-      },
-      {
-        accessorKey: "enforcement",
-        header: "Enforcement",
-        size: 120,
-      },
-      {
-        accessorFn: (row) => row.primary_officer?.name,
-        id: "primary_officer.name",
-        header: "Primary",
-        filterVariant: "multi-select",
-        filterSelectOptions: staffUserList,
-        size: 120,
-      },
-    ],
-    [inspectionStatusList, staffUserList]
-  );
-
-  return inspections && inspections.length > 0 ? (
-    <MasterDataTable
-      columns={columns}
-      data={inspections}
-      initialState={{
-        sorting: [
-          {
-            id: "ir_number",
-            desc: false,
-          },
-        ],
-      }}
-      state={{
-        isLoading: isLoading,
-        showGlobalFilter: true,
-      }}
-      enableTopToolbar={false}
-      muiTableProps={{
-        id: "case-file-inspections-table",
-      }}
-      isStackedTables
-    />
-  ) : (
-    <></>
+        })
+      ) : (
+        <Typography
+          variant="body2"
+          color={BCDesignTokens.typographyColorPlaceholder}
+        >
+          You do not have any created inspections on this file.
+        </Typography>
+      )}
+    </>
   );
 };
 
