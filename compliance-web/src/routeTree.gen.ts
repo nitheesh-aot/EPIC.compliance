24b47c566fcaa835c025c732656c07d72387e463
Tom Chapman <89718178+tom0827@users.noreply.github.com>
2026-02-25 14:14:27 -0800

    NO-ISSUE: unhiding reports (#756)

--- compliance-web/src/routeTree.gen.ts
+++ compliance-web/src/routeTree.gen.ts
@@ -18,6 +18,7 @@ import { Route as AuthenticatedAdminStaffRouteImport } from './routes/_authentic
 import { Route as AuthenticatedAdminProponentsRouteImport } from './routes/_authenticated/admin/proponents'
 import { Route as AuthenticatedAdminAgenciesRouteImport } from './routes/_authenticated/admin/agencies'
 import { Route as AuthenticatedCeDatabaseRequirementsIndexRouteImport } from './routes/_authenticated/ce-database/requirements/index'
+import { Route as AuthenticatedCeDatabaseReportsIndexRouteImport } from './routes/_authenticated/ce-database/reports/index'
 import { Route as AuthenticatedCeDatabaseInspectionsIndexRouteImport } from './routes/_authenticated/ce-database/inspections/index'
 import { Route as AuthenticatedCeDatabaseComplaintsIndexRouteImport } from './routes/_authenticated/ce-database/complaints/index'
 import { Route as AuthenticatedCeDatabaseCaseFilesIndexRouteImport } from './routes/_authenticated/ce-database/case-files/index'
@@ -74,6 +75,12 @@ const AuthenticatedCeDatabaseRequirementsIndexRoute =
     path: '/ce-database/requirements/',
     getParentRoute: () => AuthenticatedRoute,
   } as any)
+const AuthenticatedCeDatabaseReportsIndexRoute =
+  AuthenticatedCeDatabaseReportsIndexRouteImport.update({
+    id: '/ce-database/reports/',
+    path: '/ce-database/reports/',
+    getParentRoute: () => AuthenticatedRoute,
+  } as any)
 const AuthenticatedCeDatabaseInspectionsIndexRoute =
   AuthenticatedCeDatabaseInspectionsIndexRouteImport.update({
     id: '/ce-database/inspections/',
@@ -125,6 +132,7 @@ export interface FileRoutesByFullPath {
   '/ce-database/case-files/': typeof AuthenticatedCeDatabaseCaseFilesIndexRoute
   '/ce-database/complaints/': typeof AuthenticatedCeDatabaseComplaintsIndexRoute
   '/ce-database/inspections/': typeof AuthenticatedCeDatabaseInspectionsIndexRoute
+  '/ce-database/reports/': typeof AuthenticatedCeDatabaseReportsIndexRoute
   '/ce-database/requirements/': typeof AuthenticatedCeDatabaseRequirementsIndexRoute
 }
 export interface FileRoutesByTo {
@@ -141,6 +149,7 @@ export interface FileRoutesByTo {
   '/ce-database/case-files': typeof AuthenticatedCeDatabaseCaseFilesIndexRoute
   '/ce-database/complaints': typeof AuthenticatedCeDatabaseComplaintsIndexRoute
   '/ce-database/inspections': typeof AuthenticatedCeDatabaseInspectionsIndexRoute
+  '/ce-database/reports': typeof AuthenticatedCeDatabaseReportsIndexRoute
   '/ce-database/requirements': typeof AuthenticatedCeDatabaseRequirementsIndexRoute
 }
 export interface FileRoutesById {
@@ -159,6 +168,7 @@ export interface FileRoutesById {
   '/_authenticated/ce-database/case-files/': typeof AuthenticatedCeDatabaseCaseFilesIndexRoute
   '/_authenticated/ce-database/complaints/': typeof AuthenticatedCeDatabaseComplaintsIndexRoute
   '/_authenticated/ce-database/inspections/': typeof AuthenticatedCeDatabaseInspectionsIndexRoute
+  '/_authenticated/ce-database/reports/': typeof AuthenticatedCeDatabaseReportsIndexRoute
   '/_authenticated/ce-database/requirements/': typeof AuthenticatedCeDatabaseRequirementsIndexRoute
 }
 export interface FileRouteTypes {
@@ -177,6 +187,7 @@ export interface FileRouteTypes {
     | '/ce-database/case-files/'
     | '/ce-database/complaints/'
     | '/ce-database/inspections/'
+    | '/ce-database/reports/'
     | '/ce-database/requirements/'
   fileRoutesByTo: FileRoutesByTo
   to:
@@ -193,6 +204,7 @@ export interface FileRouteTypes {
     | '/ce-database/case-files'
     | '/ce-database/complaints'
     | '/ce-database/inspections'
+    | '/ce-database/reports'
     | '/ce-database/requirements'
   id:
     | '__root__'
@@ -210,6 +222,7 @@ export interface FileRouteTypes {
     | '/_authenticated/ce-database/case-files/'
     | '/_authenticated/ce-database/complaints/'
     | '/_authenticated/ce-database/inspections/'
+    | '/_authenticated/ce-database/reports/'
     | '/_authenticated/ce-database/requirements/'
   fileRoutesById: FileRoutesById
 }
@@ -284,6 +297,13 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof AuthenticatedCeDatabaseRequirementsIndexRouteImport
       parentRoute: typeof AuthenticatedRoute
     }
+    '/_authenticated/ce-database/reports/': {
+      id: '/_authenticated/ce-database/reports/'
+      path: '/ce-database/reports'
+      fullPath: '/ce-database/reports/'
+      preLoaderRoute: typeof AuthenticatedCeDatabaseReportsIndexRouteImport
+      parentRoute: typeof AuthenticatedRoute
+    }
     '/_authenticated/ce-database/inspections/': {
       id: '/_authenticated/ce-database/inspections/'
       path: '/ce-database/inspections'
@@ -341,6 +361,7 @@ interface AuthenticatedRouteChildren {
   AuthenticatedCeDatabaseCaseFilesIndexRoute: typeof AuthenticatedCeDatabaseCaseFilesIndexRoute
   AuthenticatedCeDatabaseComplaintsIndexRoute: typeof AuthenticatedCeDatabaseComplaintsIndexRoute
   AuthenticatedCeDatabaseInspectionsIndexRoute: typeof AuthenticatedCeDatabaseInspectionsIndexRoute
+  AuthenticatedCeDatabaseReportsIndexRoute: typeof AuthenticatedCeDatabaseReportsIndexRoute
   AuthenticatedCeDatabaseRequirementsIndexRoute: typeof AuthenticatedCeDatabaseRequirementsIndexRoute
 }
 
@@ -362,6 +383,8 @@ const AuthenticatedRouteChildren: AuthenticatedRouteChildren = {
     AuthenticatedCeDatabaseComplaintsIndexRoute,
   AuthenticatedCeDatabaseInspectionsIndexRoute:
     AuthenticatedCeDatabaseInspectionsIndexRoute,
+  AuthenticatedCeDatabaseReportsIndexRoute:
+    AuthenticatedCeDatabaseReportsIndexRoute,
   AuthenticatedCeDatabaseRequirementsIndexRoute:
     AuthenticatedCeDatabaseRequirementsIndexRoute,
 }
